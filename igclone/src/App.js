f680a8ba9ce5eb80653eec7f9ad5b0fc4f2e7327
ProgrammerAvinash <62152948+ProgrammerAvinash@users.noreply.github.com>
2020-07-15 23:29:49 +0530

    first commit

---
+++ igclone/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+import Post from './Post'
+
+function App() {
+  return (
+    <div className="App">
+     
+      <div className="appHeader"> 
+
+      <img 
+        className = "AppHeaderImg"
+        src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+        alt="HeaderImg"
+        />
+
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ igclone/src/Post.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Post = () => {
+    return (
+        <div className="post">
+        {`Header = > username + Avatar`}
+        {`pic`}
+        {`Avatar => `}            
+        </div>
+    );
+};
+
+export default Post;
